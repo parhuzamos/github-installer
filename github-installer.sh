617b7fcbaa92ebd485428f93ea81b03677e5057a
Bence <parhuzamos@parhuzamos.com>
2014-03-22 09:20:43 +0100

    Initial version.

---
+++ github-installer.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#rm ~/bin/apps/adblogga -rf
+defdir=~/bin/apps
+app=${1}
+
+function ask_continue() {
+	read -p  "Continue? [y/n] " -n 1 -s response
+	if [ ! "${response}" = "y" ]; then
+		echo "${response}"
+		echo
+		echo "User abort. Exiting."
+		exit 1;
+	fi
+	echo "${response}"
+	echo
+}
+
+echo "Installing ${app}..."
+echo
+read -p "Select a directory to install to (a subdirectory will be created here): [${defdir}] " ${installdir}
+if [ "${installdir}" = "" ]; then
+	installdir=${defdir}
+fi
+installdir=$(realpath ${installdir})/${app}
+echo -e "\n$app will be installed to \"${installdir}\""
+ask_continue
+#git clone https://github.com/parhuzamos/${app}.git ${installdir}
+#exitcode=$?
+exitcode=0
+if [ ! "${exitcode}" = "0" ]; then
+	echo
+	echo "Program error. Exiting."
+	exit ${exitcode}
+fi
+OIFS=IFS
+IFS=":"
+echo "Select a directory to place symlink(s):"
+select linkdir in $PATH; do
+	break;
+done
+IFS=$OIFS
+if [ "${linkdir}" = "" ]; then
+	echo "User abort. Exiting."
+	exit 1;
+fi
+lindir=$(realpath $linkdir)
+executables=""
+count=0
+for file in ${installdir}/*; do
+	if $(head -n 1 ${file} | grep "#\!/" >/dev/null); then
+		if [[ "${executables}" = "" ]]; then
+			executables=$file
+		else
+			executables=${executables}:$file
+		fi;
+		count=$((count+1))
+	fi
+done
+echo "Creating ${count} symlink(s) in \"${linkdir}\""
+ask_continue
+OIFS=IFS
+IFS=":"
+for executable in ${executables}; do
+	filename=$(basename "${executable}")
+	filename="${filename%.*}"
+	echo "Link ${executable} -> ${linkdir}/${filename}"
+	ln -s ${executable} ${linkdir}/${filename}
+done
+uninstall=${installdir}/uninstall.sh
+echo "#!/bin/bash" >${uninstall}
+echo "rm -rf ${installdir}" >>${uninstall}
+IFS=$OIFS
+OIFS=IFS
+IFS=":"
+for executable in ${executables}; do
+	filename=$(basename "${executable}")
+	filename="${filename%.*}"
+	echo "rm ${linkdir}/${filename}" >>${uninstall}
+done;
+IFS=$OIFS
+chmod +x ${uninstall}
+echo
+echo "Installation successfull."
+echo "To uninstall just execute this: "
+echo "    ${uninstall}"
+echo
+echo "Done."
